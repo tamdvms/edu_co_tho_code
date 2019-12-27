6e7804728e39e8add280cc05ac36dd718f53de1d
Tam Huynh <tam@dvms.vn>
2019-12-27 17:27:44 +0700

    final

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 # Create network for deploy
-docker network create edu_network
+#docker network create edu_network
 
 # Build api image
 docker build -f DockerfileApi -t edu_api:1.0 .
 
 # Build frontend Image
 docker build -f DockerfileFrontend -t edu_fe:1.0 . --no-cache
-
 
 # Build backend Image
 docker build -f DockerfileBackend -t edu_be:1.0 . --no-cache
