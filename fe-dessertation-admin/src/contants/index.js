018abacac50be28fab9d5074ad0a5d0d05b9d6df
Tam Huynh <tam@dvms.vn>
2019-12-27 17:31:32 +0700

    .

--- fe-dessertation-admin/src/contants/index.js
+++ fe-dessertation-admin/src/contants/index.js
@@ -1,3 +1,3 @@
 // export const HOST = 'http://localhost:1337/admin/';
-export const HOST = 'http://34.68.139.112:8081/admin/';
+export const HOST = 'http://34.68.139.112:8083/admin/';
 // export const HOST = 'http://localhost:1337/admin/';
