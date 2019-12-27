f9677b70641b858287db01733e01f30d90927d9e
Tam Huynh <tam@dvms.vn>
2019-12-27 15:52:45 +0700

    .

--- build.sh
+++ build.sh
@@ -4,6 +4,13 @@ docker network create edu_network
 # Build api image
 docker build -f DockerfileApi -t edu_api:1.0 .
 
+# Build frontend Image
+docker build -f DockerfileFrontend -t edu_fe:1.0 .
+
+
+# Build backend Image
+docker build -f DockerfileBackend -t edu_be:1.0 .
+
 # Starting system
 docker-compose -f docker-compose.yml up -d
 
