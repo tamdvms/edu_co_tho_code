85fabe43900212f01663e95c6cb758d805e0f2b5
Tam Huynh <tam@dvms.vn>
2019-12-27 15:39:22 +0700

    .

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 # Create network for deploy
 docker network create edu_network
 
-# Start mongo db
-docker-compose -f mongodb-compose.yml up -d
-
-# Wait for mongo db start complete
-sleep 10
-
-#Start api
+# Build api image
 docker build -f DockerfileApi -t edu_api:1.0 .
-docker-compose -f api-compose.yml up -d
+
+# Starting system
+docker-compose -f docker-compose.yml up -d
+
 
 
 
